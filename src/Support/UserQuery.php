4044d2b14d3be94e619995be95cf104ed7604530
Minh Bang <contact@minhbang.com>
2015-09-18 14:25:46 +0700

    Add User Query trait

---
+++ src/Support/UserQuery.php
@@ -0,0 +1,49 @@
+<?php
+namespace Minhbang\LaravelUser\Support;
+
+/**
+ * Class UserQuery
+ *
+ * @package Minhbang\LaravelKit\Traits\Model
+ * @property-read string $table
+ * @method \Illuminate\Database\Eloquent\Relations\BelongsTo belongsTo($related, $foreignKey = null, $otherKey = null, $relation = null)
+ */
+
+trait UserQuery
+{
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(config('auth.model'));
+    }
+
+    /**
+     * @param \Illuminate\Database\Query\Builder $query
+     * @return \Illuminate\Database\Query\Builder
+     */
+    public function scopeNotMine($query)
+    {
+        return $query->where("{$this->table}.user_id", '<>', user('id'));
+    }
+
+    /**
+     * @param \Illuminate\Database\Query\Builder $query
+     * @return \Illuminate\Database\Query\Builder
+     */
+    public function scopeMine($query)
+    {
+        return $query->where("{$this->table}.user_id", '=', user('id'));
+    }
+
+    /**
+     * @param \Illuminate\Database\Query\Builder $query
+     * @return \Illuminate\Database\Query\Builder
+     */
+    public function scopeWithAuthor($query)
+    {
+        return $query->leftJoin('users', 'users.id', '=', "{$this->table}.user_id")
+            ->addSelect("users.username as author");
+    }
+}
